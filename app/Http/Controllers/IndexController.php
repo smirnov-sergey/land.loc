f491778487c99c56cde1ad79b7b2a2daa734aca9
Сергей Смирнов <sergei-sm@office.taxnet.ru>
2019-12-17 09:45:40 +0300

    Cоздал модели
    Создал:
    модели,
    в контроллере Index прописал функционал для создания меню в header,
    заполнил контентом views

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
 
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
-        return view('layouts.site');
+        $pages = Page::all();
+        $portfolios = Portfolio::get('name', 'filter', 'images');
+        $services = Service::where('id', '<', 20)->get();
+        $peoples = People::take(3)->get();
+
+        // меню для header
+        $menu = [];
+
+        foreach ($pages as $page) {
+
+            $item = ['title' => $page->name, 'alias' => $page->alias];
+            array_push($menu, $item);
+        }
+
+        // 'alias' => id из view content -> <section id="service">
+        $item = ['title' => 'Services', 'alias' => 'service'];
+        array_push($menu, $item);
+
+        $item = ['title' => 'Portfolio', 'alias' => 'Portfolio'];
+        array_push($menu, $item);
+
+        $item = ['title' => 'Team', 'alias' => 'team'];
+        array_push($menu, $item);
+
+        $item = ['title' => 'Contact', 'alias' => 'contact'];
+        array_push($menu, $item);
+
+
+        return view('site.index', array(
+            'menu' => $menu,
+            'pages' => $pages,
+            'services' => $services,
+            'portfolios' => $portfolios,
+            'peoples' => $peoples,
+        ));
     }
 }
