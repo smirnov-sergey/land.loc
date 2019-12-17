38acfa572beba20f8b0bb22c499c882d7619932c
Сергей Смирнов <sergei-sm@office.taxnet.ru>
2019-12-17 13:57:14 +0300

    Страница метериала (content page)
    Добавил страницу материала

--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -30,21 +30,19 @@
                             <div class="row">
                                 <div class=" col-lg-4 col-md-4 col-sm-4 col-xs-12 pull-right">
                                     {!! Html::image('assets/img/' . $page->images, '', array('class' => 'img-circle delay-03s animated wow zoomIn')) !!}
-
                                 </div>
                                 <div class=" col-lg-7 col-md-7 col-sm-7 col-xs-12 pull-left">
                                     <div class=" delay-01s animated fadeInDown wow animated">
                                         {{ $page->text }}
                                     </div>
                                     <div class="work_bottom"><span>Want to know more..</span>
-                                        <a href="{{ route('page',array('alias'=>$page->alias)) }}"
-                                           class="contact_btn"></a>
+                                        <a href="{{ route('page', array('alias' => $page->alias)) }}"
+                                           class="contact_btn">About us</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 </div>
             </section>
             <!--Aboutus-->
